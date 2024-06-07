3e54b347d5bb3ff3ba2c43b79d7291415a0d141f
Abbas Sadriwala <abbassadri786@gmail.com>
2024-06-07 16:25:06 +0530

    Contact Managment

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+  );
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
+    toast.success("Contact added successfully!");
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+    toast.success("Contact removed successfully!");
+  };
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div className="ui container">
+      <ToastContainer />
+      <Header />
+      <AddContact addContactHandler={addContactHandler} contacts={contacts} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="ui fixed menu" style={{backgroundColor: "rgb(222, 49, 99)"}}>
+            <div className="ui container center">
+                <h2>Contact Manager</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
